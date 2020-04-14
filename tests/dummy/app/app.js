f044c998a227d02149cbe20a6074b578008519f3
Sergio del Valle <sergio.delvalle@bbvaitspain.com>
2020-04-14 16:41:23 +0200

    chore(update): update to ember 3.16

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -1,5 +1,5 @@
 import Application from '@ember/application';
-import Resolver from './resolver';
+import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
@@ -10,5 +10,3 @@ const App = Application.extend({
 });
 
 loadInitializers(App, config.modulePrefix);
-
-export default App;
