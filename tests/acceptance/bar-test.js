e9049cee3f1d7738f73cec59f346a00a2ae5d3b6
Jose Luis Represa <joseluis.represa@bbva.com>
2020-04-15 09:03:25 +0200

    test(app): fix export

--- tests/acceptance/bar-test.js
+++ tests/acceptance/bar-test.js
@@ -1,23 +1,23 @@
 /* global requirejs */
-import { module, skip } from 'qunit';
+import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | bar', (hooks) => {
 	setupApplicationTest(hooks);
 
-	skip('route exists', (assert) => {
+	test('route exists', (assert) => {
 		assert.ok(requirejs('foo/pods/bar/route'));
 	});
 
-	skip('index route exists', (assert) => {
+	test('index route exists', (assert) => {
 		assert.ok(requirejs('foo/pods/bar/index/route'));
 	});
 
-	skip('index controller exists', (assert) => {
+	test('index controller exists', (assert) => {
 		assert.ok(requirejs('foo/pods/bar/index/controller'));
 	});
 
-	skip('index template exists', (assert) => {
+	test('index template exists', (assert) => {
 		assert.ok(requirejs('foo/pods/bar/index/template'));
 	});
 });
--- tests/acceptance/component-test.js
+++ tests/acceptance/component-test.js
@@ -1,15 +1,15 @@
 /* global requirejs */
-import { module, skip } from 'qunit';
+import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | component', (hooks) => {
 	setupApplicationTest(hooks);
 
-	skip('component exists', (assert) => {
+	test('component exists', (assert) => {
 		assert.ok(requirejs('foo/components/test-component/component'));
 	});
 
-	skip('template exists', (assert) => {
+	test('template exists', (assert) => {
 		assert.ok(requirejs('foo/components/test-component/template'));
 	});
 });
